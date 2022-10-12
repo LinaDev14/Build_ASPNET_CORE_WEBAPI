1f6bf5747e703c1555962fa91d7ab11a84262b48
Linadev14 <lina.guerrero@inchcape.com>
2022-10-12 12:15:03 -0500

    Migration and update Dbcontext to SqlServer

---
+++ LinaDev.API/Data/LinaDevWalksDbContext.cs
@@ -0,0 +1,20 @@
+﻿using LinaDev.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace LinaDev.API.Data
+{
+    public class LinaDevWalksDbContext : DbContext
+    {
+
+        public LinaDevWalksDbContext(DbContextOptions<LinaDevWalksDbContext> options)
+            : base(options)
+        {
+
+        }
+
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
